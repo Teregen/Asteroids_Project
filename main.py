97ed23256cc4ef1f8a25440ddb147dd7cd1bfc17
Teregen <jjbbailey@gmail.com>
2025-03-21 19:38:43 -0700

    Added player class and player icon, can move it, and added math for moving, and keyboard controls

--- main.py
+++ main.py
@@ -3,36 +3,30 @@
 #throughout this file
 import pygame
 from constants import * ##import constants from constants.py
-
+from circleshape import CircleShape
+from player import Player
 
 
 def main():
     pygame.init() #initialize the pygame library
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT)) #create a window
+    dt = 0
+    clock = pygame.time.Clock() #create a clock object to track time
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, PLAYER_RADIUS) #create a player object
+
+    
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return #exit the game loop if the user closes the window
         screen.fill("black")
+        player.draw(screen)
+        player.update(dt)
         pygame.display.flip() #update the display, need pygame first
-
-
-
-
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
-
-
-
-
-
-
-
-
-
-
+        clock.tick(60) #limit the frame rate to 60 FPS
+        dt = clock.get_time() / 1000 #get the time since the last frame in seconds
+    
 
 if __name__ == "__main__":
     main()
